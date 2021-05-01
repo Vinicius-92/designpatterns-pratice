a2b58fe50b30ba0433654d1661e4720a76d761fe
Vinicius Augusto <vinicius_augusto_@msn.com>
2021-05-01 16:35:07 -0300

    Functional approach to builder pattern

--- builder-functional/src/model/FastFoodMeal.java
+++ builder-functional/src/model/FastFoodMeal.java
@@ -2,11 +2,11 @@ package model;
 
 public class FastFoodMeal {
 
-    private String drink;
-    private String main;
-    private String side;
-    private String dessert;
-    private String gift;
+    final private String main;
+    final private String drink;
+    final private String side;
+    final private String dessert;
+    final private String gift;
 
     public FastFoodMeal(String drink, String main, String side, String dessert, String gift) {
         this.drink = drink;
@@ -39,11 +39,50 @@ public class FastFoodMeal {
     @Override
     public String toString() {
         return "FastFoodMeal{" +
-                "drink='" + drink + '\'' +
-                ", main='" + main + '\'' +
+                "main='" + main + '\'' +
+                ", drink='" + drink + '\'' +
                 ", side='" + side + '\'' +
                 ", dessert='" + dessert + '\'' +
                 ", gift='" + gift + '\'' +
                 '}';
+    }
+
+    public static class Builder {
+
+        // Mandatory
+        final private String main;
+        //Optional
+        private String drink;
+        private String side;
+        private String dessert;
+        private String gift;
+
+        public Builder(String main) {
+            this.main = main;
+        }
+
+        public Builder addDrink(String drink) {
+            this.drink = drink;
+            return this;
+        }
+
+        public Builder addSide(String side) {
+            this.side = side;
+            return this;
+        }
+
+        public Builder addDessert(String dessert) {
+            this.dessert = dessert;
+            return this;
+        }
+
+        public Builder addGift(String gift) {
+            this.gift = gift;
+            return this;
+        }
+
+        public FastFoodMeal mealReady() {
+            return new FastFoodMeal(main, drink, side, dessert, gift);
+        }
     }
 }
